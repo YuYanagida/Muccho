4ac189bcf98e559ca2441d7e8671229f57f7d16b
raytrek05 <raytrek05@kyoto-seika.ac.jp>
2022-09-02 16:40:04 +0900

    リザルト画面追加

--- Assets/RangeNotes/ScoreCounter.cs
+++ Assets/RangeNotes/ScoreCounter.cs
@@ -4,14 +4,20 @@ using UnityEngine.UI;
 public class ScoreCounter : MonoBehaviour
 {
     public static int score;
-    //public Text scoreDisplay;
+    /*public Text scoreDisplay;
     public AudioSource bad;
     public AudioSource good;
     public AudioSource great;
     public AudioClip[] Bad;
     public AudioClip[] Good;
     public AudioClip[] Great;
-    public GameObject clearcamvas;
+    public GameObject clearcamvas;*/
+    public GameObject resultB;
+    public GameObject resultBB;
+    public GameObject resultA;
+    public GameObject resultAA;
+    public GameObject resultS;
+    public GameObject resultSS;
 
     public static void AddScore(int point)
     {
@@ -23,29 +29,53 @@ public class ScoreCounter : MonoBehaviour
 
     public void TotalScore()
     {
-        clearcamvas.SetActive(true);
+       // clearcamvas.SetActive(true);
 
-        if (score < 5)
+        if (score < 3)
         {
             Debug.Log("励ましのセリフです");
 
-            bad.PlayOneShot(Bad[Random.Range(0, 1)]);
-            
+            // bad.PlayOneShot(Bad[Random.Range(0, 1)]);
+
+            resultB.SetActive(true);
+
 
         }
 
-        else if (score >= 5 && score < 20)
+        else if (score >= 3 && score < 5)
+        {
+            Debug.Log("励ましのセリフです");
+
+            resultBB.SetActive(true);
+        }
+
+        else if (score >= 5 && score < 15)
         {
             Debug.Log("いい感じのセリフです");
 
-            good.PlayOneShot(Good[Random.Range(0, 1)]);
+            resultA.SetActive(true);
         }
 
-        else if (score >= 20)
+        else if (score >= 15 && score < 20)
+        {
+            Debug.Log("いい感じのセリフです");
+
+            resultAA.SetActive(true);
+        }
+
+
+        else if (score >= 20 && score < 40)
         {
             Debug.Log("褒めるのセリフです");
 
-            great.PlayOneShot(Great[Random.Range(0, 1)]);
+            resultS.SetActive(true);
+        }
+
+        else if (score >= 40)
+        {
+            Debug.Log("褒めるのセリフです");
+
+            resultSS.SetActive(true);
         }
 
     }
